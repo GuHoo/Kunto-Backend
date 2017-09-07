532bd2b5fca1a19fb8b6e53b1af655c7a8eec847
MaxMEllon <maxmellon1994@gmail.com>
2017-09-07 11:45:35 +0900

    Modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,18 @@ hukkin = Part.create(name: '腹筋')
 kyoukin = Part.create(name: '胸筋')
 jyouwannitoukin = Part.create(name: '上腕二頭筋')
 
-kuranti = Train.new(name: 'クランチ', detail: '一般的な腹筋トレーニング')
+crunch = Train.new(name: 'クランチ', detail: '一般的な腹筋トレーニング')
 
-kuranti.parts.push hukkin
-kuranti.parts.push kyoukin
-kuranti.parts.push jyouwannitoukin
+crunch.parts.push hukkin
+crunch.parts.push kyoukin
+crunch.parts.push jyouwannitoukin
 
-kuranti.save
+crunch.save
 
 sample = Menu.create(name: 'サンプルトレーニング')
 
 user = User.new(email: 'sample@hoge.com', password: 'hogehoge', password_confirmation: 'hogehoge')
 
-MenuTrain.create(menu_id: sample.id, train_id: kuranti.id, user_id: user.id)
+UserTrain.create(user_id: user.id, train_id: crunch.id, count: 100)
+
+MenuTrain.create(menu_id: sample.id, train_id: crunch.id, user_id: user.id)
