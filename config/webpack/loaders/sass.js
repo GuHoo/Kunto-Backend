d773bcc948aa1b8802ab06405da446364f9095e3
MaxMEllon <maxmellon1994@gmail.com>
2017-09-09 14:28:21 +0900

    Active css module.

--- config/webpack/loaders/sass.js
+++ config/webpack/loaders/sass.js
@@ -6,10 +6,27 @@ module.exports = {
   use: ExtractTextPlugin.extract({
     fallback: 'style-loader',
     use: [
-      { loader: 'css-loader', options: { minimize: env.NODE_ENV === 'production' } },
-      { loader: 'postcss-loader', options: { sourceMap: true } },
+      {
+        loader: 'css-loader',
+        options: {
+          minimize: env.NODE_ENV === 'production',
+          modules: true,
+          localIdentName: '[path][name]__[local]--[hash:base64:5]',
+        }
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true
+        }
+      },
       'resolve-url-loader',
-      { loader: 'sass-loader', options: { sourceMap: true } }
+      {
+        loader: 'sass-loader',
+        options: {
+          sourceMap: true
+        }
+      }
     ]
   })
 }
