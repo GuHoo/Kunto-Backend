ef40a62093b1ccb8948f452fd08c8b1d234b19d5
MaxMEllon <maxmellon1994@gmail.com>
2017-09-12 00:54:11 +0900

    Disable logging when production.

--- app/javascript/packs/stores/index.js
+++ app/javascript/packs/stores/index.js
@@ -1,18 +1,15 @@
 import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from '../reducers';
 import rootSaga from '../sagas';
 
 export default function configureStore(initialState) {
+  const middlewares = [];
   const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(
-    reducer,
-    initialState,
-    applyMiddleware(
-      sagaMiddleware, createLogger()
-    )
-  );
+  middlewares.push(sagaMiddleware);
+  if (process.env.NODE_ENV !== 'production') middlewares.push(createLogger());
+  const store = reducer >> compose(applyMiddleware(...middlewares))(createStore);
   sagaMiddleware.run(rootSaga);
   return store;
 }
