d2a0b751860204b80297f86c37d0b4f1bb7e85a2
MaxMEllon <maxmellon1994@gmail.com>
2017-09-11 14:38:42 +0900

    oops

--- app/javascript/packs/components/my/index.js
+++ app/javascript/packs/components/my/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import autoBind from 'react-autobind';
-import Human from './Human';
 
 export default function My() {
   return (
     <div className="row">
       <div className="col s1 m1" />
-      <div className="col s10 m10">
-        <Human />
-      </div>
+      <div className="col s10 m10" />
       <div className="col s1 m1" />
     </div>
   );
