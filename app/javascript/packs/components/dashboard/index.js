03f3705181b95875d1acc784854c8b9f03ffadc6
MaxMEllon <maxmellon1994@gmail.com>
2017-09-12 00:05:04 +0900

    Add Body in dashbord.

--- app/javascript/packs/components/dashboard/index.js
+++ app/javascript/packs/components/dashboard/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Body from './Body';
 
 export default function Dashboard() {
   return (
     <div>
       <Header />
+      <Body />
       <Footer />
     </div>
   );
